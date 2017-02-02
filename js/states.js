710eeef523769e4a11d8db99a690e8488cd82410
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-02-02 19:05:38 +0100

    played arround with scroll spy - found out that the data-offset is for scrollspy, but to handle the actual jumping to the #id I need to play with the scrolling in javascript - see the controller of the eq1 state.

--- js/states.js
+++ js/states.js
@@ -16,6 +16,7 @@
             controller: [ '$scope', '$location', '$anchorScroll', function ($scope, $location, $anchorScroll) {
                 $scope.goToAnchor = function (name){
                     $location.hash(name);
+                    $anchorScroll.yOffset = 70; //as angularstrap's scrollspy data-offset is only for spying, I have to manage the actual jump to be offseted by 70px, as the navigation bar is affixed and height 60.
                     $anchorScroll();
                 }
                 
